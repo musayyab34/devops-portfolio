165ce0fd66417d3873cb4e41525da87a14baba59
Musayyab <musa@musa.localdomain>
2025-12-24 18:16:00 +0500

    Test Edit

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -40,12 +40,12 @@ type TConfig = {
 
 export const config: TConfig = {
   html: {
-    title: "John Doe — 3D Portfolio",
-    fullName: "John Doe",
+    title: "Musa — 3D Portfolio",
+    fullName: "Musa",
     email: "johndoe@mail.com",
   },
   hero: {
-    name: "John Doe",
+    name: "Musa",
     p: ["I develop 3D visuals, user", "interfaces and web applications"],
   },
   contact: {
